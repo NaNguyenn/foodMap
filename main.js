bf9d3759800b6c78c2aa23092f61254c1021d118
NguyennNguyenn <yu19151998@gmail.com>
2021-08-12 21:03:25 +0700

    Add food icons

--- main.js
+++ main.js
@@ -8,15 +8,51 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-var myIcon = L.icon({
+var iconBe = L.icon({
     iconUrl: "img/be.png",
+    iconSize: [90, 90],
+    iconAnchor: [45, 45],
+    popupAnchor: [-3, -76],
+});
+
+var iconBeo = L.icon({
+    iconUrl: "img/beo.png",
     iconSize: [80, 80],
     iconAnchor: [40, 40],
     popupAnchor: [-3, -76],
 });
 
-var marker = L.marker([21.038128, 105.822291], { icon: myIcon }).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+var iconFood = L.Icon.extend({
+    options: {
+        iconSize: [80, 80],
+        iconAnchor: [40, 40],
+        popupAnchor: [-3, -76],
+    }
+});
+
+var iconPho = new iconFood({ iconUrl: "img/pho.png" }),
+    iconBun = new iconFood({ iconUrl: "img/bun.png" }),
+    iconBanhGato = new iconFood({ iconUrl: "img/banhGato.png" }),
+    iconBanhMi = new iconFood({ iconUrl: "img/banhMi.png" }),
+    iconCafe = new iconFood({ iconUrl: "img/cafe.png" }),
+    iconcafeMeo = new iconFood({ iconUrl: "img/cafeMeo.png" }),
+    iconCuon = new iconFood({ iconUrl: "img/cuon.png" }),
+    iconHotdog = new iconFood({ iconUrl: "img/hotdog.png" }),
+    iconKem = new iconFood({ iconUrl: "img/kem.png" }),
+    iconNemChua = new iconFood({ iconUrl: "img/nemChua.png" }),
+    iconNgocThach = new iconFood({ iconUrl: "img/ngocThach.png" }),
+    iconNuong = new iconFood({ iconUrl: "img/nuong.png" }),
+    iconOc = new iconFood({ iconUrl: "img/oc.png" }),
+    iconPizza = new iconFood({ iconUrl: "img/pizza.png" }),
+    iconSpaghetti = new iconFood({ iconUrl: "img/spaghetti.png" }),
+    iconTaco = new iconFood({ iconUrl: "img/taco.png" }),
+    iconTrung = new iconFood({ iconUrl: "img/trung.png" });
+
+var markerBe = L.marker([21.037853, 105.821825], { icon: iconBe }).addTo(mymap);
+markerBe.bindPopup("<b>Hello world!</b><br>I am a popup.");
+
+var markerBeo = L.marker([21.043056, 105.784553], { icon: iconBeo }).addTo(mymap);
+markerBeo.bindPopup("<b>Hello world!</b><br>I am a popup.");
 
 function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
